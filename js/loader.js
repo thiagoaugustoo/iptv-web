2965aa8e2643ac7c1aa5f2d4fab3508ac3c27a91
thiago augusto ferreira da silva <thiagoaugusto1976@gmail.com>
2026-06-07 18:57:26 -0300

    1.0.20

--- js/loader.js
+++ js/loader.js
@@ -1,21 +1,21 @@
 const version = Date.now(); // ou build hash
 
 const scripts = [
-  "js/utils.js",
-  "js/storage.js",
-  "js/parser.js",
-  "js/search.js",
-  "js/navigation.js",
-  "js/player.js",
-  "js/ui/home.js",
-  "js/ui/livetv.js",
-  "js/ui/movies.js",
-  "js/ui/series.js",
-  "js/ui/searchpage.js",
-  "js/ui/favorites.js",
-  "js/ui/history.js",
-  "js/ui/settings.js",
-  "js/app.js",
+  "/js/utils.js",
+  "/js/storage.js",
+  "/js/parser.js",
+  "/js/search.js",
+  "/js/navigation.js",
+  "/js/player.js",
+  "/js/ui/home.js",
+  "/js/ui/livetv.js",
+  "/js/ui/movies.js",
+  "/js/ui/series.js",
+  "/js/ui/searchpage.js",
+  "/js/ui/favorites.js",
+  "/js/ui/history.js",
+  "/js/ui/settings.js",
+  "/js/app.js",
   "https://cdn.jsdelivr.net/npm/hls.js@latest/dist/hls.min.js"
 ];
 
