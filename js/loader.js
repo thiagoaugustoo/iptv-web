dd8811785a1e3603a7874d7541477cbd7faf34b8
thiago augusto ferreira da silva <thiagoaugusto1976@gmail.com>
2026-06-07 19:04:05 -0300

    1.0.21

--- js/loader.js
+++ js/loader.js
@@ -1,26 +1,25 @@
-const version = Date.now(); // ou build hash
+const BASE = "/js/";
 
 const scripts = [
-  "/js/utils.js",
-  "/js/storage.js",
-  "/js/parser.js",
-  "/js/search.js",
-  "/js/navigation.js",
-  "/js/player.js",
-  "/js/ui/home.js",
-  "/js/ui/livetv.js",
-  "/js/ui/movies.js",
-  "/js/ui/series.js",
-  "/js/ui/searchpage.js",
-  "/js/ui/favorites.js",
-  "/js/ui/history.js",
-  "/js/ui/settings.js",
-  "/js/app.js",
-  "https://cdn.jsdelivr.net/npm/hls.js@latest/dist/hls.min.js"
+  "utils.js",
+  "storage.js",
+  "parser.js",
+  "search.js",
+  "navigation.js",
+  "player.js",
+  "app.js",
+  "ui/home.js",
+  "ui/livetv.js",
+  "ui/movies.js",
+  "ui/series.js",
+  "ui/searchpage.js",
+  "ui/favorites.js",
+  "ui/history.js",
+  "ui/settings.js"
 ];
 
-scripts.forEach(src => {
+scripts.forEach(file => {
   const s = document.createElement("script");
-  s.src = `${src}?v=${version}`;
+  s.src = BASE + file + "?v=" + Date.now();
   document.head.appendChild(s);
 });
